5f5093bfc2f252e7e25e3b885d6898179f492495
Jeff Wendling <jeff@storj.io>
2020-05-28 15:41:46 -0600

    tests: add coverage around context errors
    Change-Id: I7534a2dfba86efcd7e6a99851162b53c2b24dbfc

--- internal/integration/error_test.go
+++ internal/integration/error_test.go
@@ -5,6 +5,7 @@ package integration
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/zeebo/assert"
@@ -26,5 +27,28 @@ func TestError(t *testing.T) {
 		assert.Nil(t, out)
 		assert.Error(t, err)
 		assert.Equal(t, drpcerr.Code(err), i)
+	}
+}
+
+func TestError_Context(t *testing.T) {
+	ctx := drpcctx.NewTracker(context.Background())
+	defer ctx.Wait()
+	defer ctx.Cancel()
+
+	cli, close := createConnection(impl{
+		Method1Fn: func(ctx context.Context, in *In) (*Out, error) {
+			return nil, [...]error{
+				context.Canceled,
+				context.DeadlineExceeded,
+			}[in.In%2]
+		},
+	})
+	defer close()
+
+	for i := int64(2); i < 20; i++ {
+		out, err := cli.Method1(ctx, in(i))
+		assert.Nil(t, out)
+		assert.Error(t, err)
+		assert.That(t, strings.Contains(err.Error(), "context"))
 	}
 }
